3f9d0ceb93a4b7511c0d905daa6db2005ade631f
Andrew Truex <andrewtruex@gmail.com>
2025-08-31 05:14:28 -0400

    fix main.py import

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import os
 # Add the parent directory to the path so we can import mcp_obsidian
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from . import main
+from mcp_obsidian import main
 
 if __name__ == "__main__":
     main()
